8df5c6ae957db30ba28cbdf063a23eace2bf28bb
Misslin Quentin <misslin.quentin@gmail.com>
2017-11-22 23:49:40 +0100

    Script Mouvement Eau
    Ajout d'un script permettant une simulation du mouvement de l'eau.
    
    Les mouvements de l'eau ne sont pas réalistes car ils consistent simplement en un changement de hauteur d'une surface sur laquel un shader Glass est appliqué (dont la distortion évolue dans le temps).
    
    Pour des raisons évidentes de performances j'ai préféré simplifier l'eau à ce simple rendu.

---
+++ Assets/Script/SCR_WaterMove.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SCR_WaterMove : MonoBehaviour {
+
+	public Vector3 speed = new Vector3(0.01f,1.5f,0f);
+	public float waveHeight =  0.5f;
+
+	Material m_Material;
+	Vector2 textureOffset;
+	Vector3 position;
+	float time;
+	float initYPos;
+
+	void Start () {
+
+		/* Init var */
+		m_Material = GetComponent<Renderer>().material;
+		textureOffset = m_Material.GetTextureOffset("_BumpMap");
+
+		position = transform.position;
+		initYPos = position.y;
+		time = 0f;
+	}
+
+	void Update () {
+		/* Wave speed direction */
+		textureOffset.x += speed.x * Time.deltaTime;
+		textureOffset.y += speed.z * Time.deltaTime;
+		m_Material.SetTextureOffset("_BumpMap", textureOffset);
+		
+		/* Wave speed height */
+		time = time + speed.y * Time.deltaTime;
+		position.y = initYPos + Mathf.Sin(time) * waveHeight;
+		transform.position = position;
+	}
+}
